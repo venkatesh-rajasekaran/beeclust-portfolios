5819f6d72a211ecaac3a47b4735a72101a1baebd
Vnkt-esh <venkatesh.rajasekaran@gmail.com>
2021-11-13 13:18:49 +0530

    remove beeclust web code from portfolios

--- DigitalSolutions/src/app/app.component.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-
-@Injectable()
-export class AppComponentService {
-    selectedServiceOfferedCard = new Subject<string>();
-    constructor() {        
-    }
-}
